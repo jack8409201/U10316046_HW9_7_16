1896a915b4687f48d1d2b225628cbf0185d3e3c2
jack8409201 <jack8409201@gmail.com>
2015-05-21 23:36:39 +0800

    Create TestSort class
    建一個排序的class呼叫sort method,先用氣泡排序法由小到大排序二為陣列的第一行,第二行跟著第一行跑

---
+++ TestSort.java
@@ -0,0 +1,29 @@
+
+public class TestSort {
+	public static void main(String[] args) {
+		int[][] m = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
+		sort(m);
+	}
+	
+	public static void sort(int m[][]){
+		
+		for (int i  = 0; i < m.length-1; i++){
+			for(int j = 0; j <= m.length-2; j++){
+				if(m[j][0] > m[j+1][0]){
+					int temp = m[j][0];
+					m[j][0] = m[j+1][0];
+					m[j+1][0] = temp;
+					
+					temp = m[j][1];
+					m[j][1] = m[j+1][1];
+					m[j+1][1] = temp;
+				}
+			}
+		}
+		
+		for (int i  = 0; i < m.length; i++){
+			System.out.println("{" + m[i][0] + "," + m[i][1] + "}");
+		}
+	}
+
+}
