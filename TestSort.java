3e25bbeef06b37d3212242923fb4250dfbd58a7e
jack8409201 <jack8409201@gmail.com>
2015-05-22 00:46:03 +0800

    Modify TestSort class
    新增註解

--- TestSort.java
+++ TestSort.java
@@ -1,12 +1,20 @@
-
+/**
+ * Name: Jou Dan-Haur
+ * ID: U10316046
+ * Ex: 7.16
+ */
 public class TestSort {
+	//Main method
 	public static void main(String[] args) {
 		int[][] m = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
+		//Call sort method
 		sort(m);
 	}
 	
+	//Sort method to arrange m array
 	public static void sort(int m[][]){
 		
+		//Using bubble sort to arrange m array first column 
 		for (int i  = 0; i < m.length-1; i++){
 			for(int j = 0; j <= m.length-2; j++){
 				if(m[j][0] > m[j+1][0]){
@@ -20,6 +28,7 @@ public class TestSort {
 				}
 			}
 		}
+		//Using bubble sort to arrange m array second column 
 		for(int i = 0; i < m.length-1; i++){
 			for(int j = 0; j <= m.length-2; j++){
 				if(m[j][0] == m[j+1][0] && m[j][1] > m[j+1][1]){
@@ -29,7 +38,7 @@ public class TestSort {
 				}
 			}
 		}
-		
+		//Print m array
 		for (int i  = 0; i < m.length; i++){
 			System.out.println("{" + m[i][0] + "," + m[i][1] + "}");
 		}
